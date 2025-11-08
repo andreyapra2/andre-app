01df178aa9efd85e75511de1baf200a36d505378
andreyapra <andreyapra@gmail.com>
2025-11-08 13:58:01 +0000

    2025-11-08

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,9 +2,17 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-25.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.go
+    pkgs.bun
+    pkgs.uv
+    pkgs.awscli2
+    pkgs.nodejs_22
+    pkgs.python313
+    pkgs.php84
+    pkgs.php84Packages.composer
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
